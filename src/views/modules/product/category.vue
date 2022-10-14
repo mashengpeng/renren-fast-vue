d184164cf0794bf0ea9e7872322e6acab6030f8a
mashengpeng <1360311705@qq.com>
2022-10-15 04:14:27 +0800

    feature:add and edit category

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,26 +1,65 @@
 <template>
-    <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick" :expand-on-click-node="false"
-        show-checkbox node-key="catId" :default-expanded-keys="defaultExpandKey">
-        <span class="custom-tree-node" slot-scope="{ node, data }">
-            <span>{{ node.label }}</span>
-            <span>
-                <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)">
-                    Append
-                </el-button>
-                <el-button v-if="node.childNodes.length == 0" type="text" size="mini" @click="() => remove(node, data)">
-                    Delete
-                </el-button>
+    <div>
+        <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick" :expand-on-click-node="false"
+            show-checkbox node-key="catId" :default-expanded-keys="defaultExpandKey">
+            <span class="custom-tree-node" slot-scope="{ node, data }">
+                <span>{{ node.label }}</span>
+                <span>
+                    <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)">
+                        Append
+                    </el-button>
+                    <el-button type="text" size="mini" @click="edit(data)">
+                        Edit
+                    </el-button>
+                    <el-button v-if="node.childNodes.length == 0" type="text" size="mini"
+                        @click="() => remove(node, data)">
+                        Delete
+                    </el-button>
+                </span>
             </span>
-        </span>
-    </el-tree>
+        </el-tree>
+        <el-dialog :title="title" :visible.sync="dialogVisible" width="30%" :close-on-click-modal="false">
+            <el-form :model="category">
+                <el-form-item label="分类名称">
+                    <el-input v-model="category.name" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="图标">
+                    <el-input v-model="category.icon" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="计量单位">
+                    <el-input v-model="category.productUnit" autocomplete="off"></el-input>
+                </el-form-item>
+
+
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="submitData">确 定</el-button>
+            </span>
+        </el-dialog>
+    </div>
+
 </template>
 
 <script>
 export default {
     data() {
         return {
+            dialogType: "",
             menus: [],
+            title: "",
             defaultExpandKey: [],
+            dialogVisible: false,
+            category: {
+                catId: "",
+                name: "",
+                parentCid: 0,
+                catLevel: 0,
+                showStatus: 1,
+                sort: 0,
+                icon: "",
+                productUnit: "",
+            },
             defaultProps: {
                 children: "children",
                 label: "name",
@@ -28,6 +67,7 @@ export default {
         }
     },
     methods: {
+
         handleNodeClick(data) {
         },
         getMenus() {
@@ -39,8 +79,81 @@ export default {
                 this.menus = data.data
             })
         },
+        submitData() {
+            if (this.dialogType == "append") {
+                this.addCategory()
+            } else if (this.dialogType == "edit") {
+                this.editCategory()
+            }
+        },
+
         append(data) {
-            console.log(data)
+            this.dialogType = "append"
+            this.dialogVisible = true
+            this.title = "增加分类"
+
+            this.category.catId = null
+            this.category.name = ""
+            this.category.parentCid = data.catId
+            this.category.catLevel = data.catLevel * 1 + 1
+            this.category.icon = ""
+            this.category.productUnit = ""
+            this.category.sort = 0
+            this.category.showStatus = 1
+        },
+
+        addCategory() {
+            this.$http({
+                url: this.$http.adornUrl('/product/category/save'),
+                method: 'post',
+                data: this.$http.adornData(this.category, false)
+            }).then(({ data }) => {
+                this.$message({
+                    message: '添加成功',
+                    type: 'success'
+                });
+                this.dialogVisible = false
+                this.defaultExpandKey = [this.category.parentCid]
+                this.getMenus()
+            });
+        },
+
+        edit(data) {
+            this.dialogType = "edit"
+            this.dialogVisible = true
+            this.title = "修改分类"
+
+
+            this.$http({
+                url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+                method: 'get',
+            }).then(({ data }) => {
+
+                this.category.catId = data.data.catId
+                this.category.name = data.data.name
+                this.category.parentCid = data.data.parentCid
+                this.category.catLevel = data.data.catLevel
+                this.category.icon = data.data.icon
+                this.category.productUnit = data.data.productUnit
+
+            })
+        },
+
+        editCategory() {
+            var { catId, name, icon, productUnit } = this.category
+            this.$http({
+                url: this.$http.adornUrl('/product/category/update'),
+                method: 'post',
+                data: this.$http.adornData({ catId, name, icon, productUnit }, false)
+            }).then(({ data }) => {
+                this.$message({
+                    message: '修改成功',
+                    type: 'success'
+                });
+                this.dialogVisible = false
+                this.defaultExpandKey = [this.category.parentCid]
+                this.getMenus()
+            });
         },
 
         remove(node, data) {
@@ -57,7 +170,7 @@ export default {
                     data: this.$http.adornData(ids, false)
                 }).then(({ data }) => {
                     this.$message({
-                        message: '删除成功', 
+                        message: '删除成功',
                         type: 'success'
                     });
                     this.defaultExpandKey = [node.parent.data.catId]
