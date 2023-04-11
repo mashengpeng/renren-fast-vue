4f371f274b2cdfc99f7242252245b6ab97cc76d7
mashengpeng <1360311705@qq.com>
2023-04-11 22:45:48 +0800

    75、商品服务-API-品牌管理-品牌分类关联与级联更新

--- src/views/modules/common/category.vue
+++ src/views/modules/common/category.vue
@@ -1,10 +1,16 @@
 <template>
-  <el-tree ref="menuTree"
-           :data="menus"
-           :props="defaultProps"
-           node-key="catId"
-           @node-click="nodeClick">
-  </el-tree>
+  <div>
+    <el-input placeholder="输入关键字进行过滤" v-model="filterText"></el-input>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      node-key="catId"
+      ref="menuTree"
+      @node-click="nodeclick"
+      :filter-node-method="filterNode"
+      :highlight-current = "true"
+    ></el-tree>
+  </div>
 </template>
 
 <script>
@@ -16,32 +22,44 @@ export default {
   components: {},
   props: {},
   data() {
+    //这里存放数据
     return {
+      filterText: "",
       menus: [],
-      defaultExpandKey: [],
+      expandedKey: [],
       defaultProps: {
         children: "children",
-        label: "name",
+        label: "name"
       }
-    }
+    };
   },
   //计算属性 类似于data概念
   computed: {},
   //监控data中的数据变化
-  watch: {},
+  watch: {
+    filterText(val) {
+      this.$refs.menuTree.filter(val);
+    }
+  },
   //方法集合
   methods: {
+    //树节点过滤
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.name.indexOf(value) !== -1;
+    },
     getMenus() {
       this.$http({
-        url: this.$http.adornUrl('/product/category/list/tree'),
-        method: 'get',
-        params: this.$http.adornParams({})
-      }).then(({data}) => {
-        this.menus = data.data
-      })
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get"
+      }).then(({ data }) => {
+        this.menus = data.data;
+      });
     },
-    nodeClick(data, node, component) {
-      this.$emit("tree-node-click", data, node, component)
+    nodeclick(data, node, component) {
+      console.log("子组件category的节点被点击", data, node, component);
+      //向父组件发送事件；
+      this.$emit("tree-node-click", data, node, component);
     }
   },
   //生命周期 - 创建完成（可以访问当前this实例）
@@ -49,24 +67,16 @@ export default {
     this.getMenus();
   },
   //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {
-  },
-  beforeCreate() {
-  }, //生命周期 - 创建之前
-  beforeMount() {
-  }, //生命周期 - 挂载之前
-  beforeUpdate() {
-  }, //生命周期 - 更新之前
-  updated() {
-  }, //生命周期 - 更新之后
-  beforeDestroy() {
-  }, //生命周期 - 销毁之前
-  destroyed() {
-  }, //生命周期 - 销毁完成
-  activated() {
-  } //如果页面有keep-alive缓存功能，这个函数会触发
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
 };
 </script>
-<style scoped>
+<style scoped>
 
 </style>
